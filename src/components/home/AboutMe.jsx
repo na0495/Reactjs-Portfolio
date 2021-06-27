0b0a455cc3d658025cbf604d0a09b66d5213e00c
na4095 <saadmrabet008@gmail.com>
2021-06-27 22:08:44 +0100

    update about me

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -1,18 +1,22 @@
 import FadeInWhenVisible from '../Animation/FadeInWhenVisible';
-import { Heading, Box, Text, Stack, useColorMode, Center } from "@chakra-ui/react"
+import { Heading, Box, Text, Stack, useColorMode, Center, Flex, Avatar } from "@chakra-ui/react"
+import main from '../../assets/main.jpeg'
   
-function Feature({ title, desc, ...rest }) {
+function Feature({ title, desc, imageUrl, imageAlt, ...rest }) {
   return (
     <Center>
       <Box p={5} boxShadow="dark-lg" shadow="md" borderWidth="1px" w="60%"{...rest} borderRadius="16" >
+
         <Stack direction={["column", "row"]} spacing="24px">
-          {/* <Box >
-            <Image objectFit="cover" borderRadius="full" boxSize="150px" src={imageUrl} alt={imageAlt}/>
-          </Box> */}
-          <Box >
-            <Heading fontSize="xl">{title}</Heading>
-            <Text mt={4}>{desc}</Text>
-          </Box>
+          <Flex>
+            <Box w="25%" >
+              <Avatar /* sx={{ verticalAlign: 'middle' }} */ size="2xl" src={main} name={imageAlt}/>
+            </Box>
+            <Box w="75%">
+              <Heading fontSize="xl">{title}</Heading>
+              <Text mt={4}>{desc}</Text>
+            </Box>
+          </Flex>  
         </Stack>
       </Box>
     </Center>
@@ -26,8 +30,12 @@ export default function AboutMe() {
         <Stack spacing={12} p={6}>
           <Feature
             backgroundColor={`mode.${colorMode}.background`}
-            title="Full-Stack web dev"
-            desc="Development of Web application for shipping."
+            title="About me"
+            desc="As you can see, my name is Mrabet saâd, a 23 yo software developer, with a bachelor's
+            Computer Science, i enjoy being challenged and engaging with projects that require me to work
+            outside my comfort zone and knowledge set, as continuing to learn new languages and developmenet techeniques."
+            imageUrl="url(assets/main.jpeg)"
+            imageAlt="Mrabet Saad"
           />
         </Stack>
       </FadeInWhenVisible>
