04df20a1cc548e7f1738e4ead3d7bee5e1fe2263
na4095 <saadmrabet008@gmail.com>
2021-07-03 23:34:54 +0100

    update lading page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,7 @@
 // import * as React from "react";
 import { Box } from '@chakra-ui/layout';
 import Hero from './home/Hero';
-import AboutMe from './home/AboutMe';
+// import AboutMe from './home/AboutMe';
 import HomeContent from './home/HomeContent';
 
 export default function LandingPage() {
@@ -9,7 +9,7 @@ export default function LandingPage() {
         <>
         <Box>
            <Hero/>
-           <AboutMe/>
+           {/* <AboutMe/> */}
            <HomeContent/>
         </Box>
         </>
--- src/components/home/HomeContent.jsx
+++ src/components/home/HomeContent.jsx
@@ -12,26 +12,26 @@ export default function HomeContent(){
                 </Heading>
                 {/* <Box > */}
                 <Text fontSize="lg" p={5}>
-                    <span className='icons'>&#129409;</span> As you can see, my name is Mrabet saâd, a 23 years old, from Tangier morocco.
+                    <span className='icons'>&#129333;</span> As you can see, my name is Mrabet saâd, a 23 years old, from Tangier morocco.
                     <br/>
                     <br/>
-                    <span className='icons'>&#128196;</span> I have a bachelor of science degree in Computer Engineering.
+                    <span className='icons'>&#127891;</span> I have a bachelor of science degree in Computer Engineering.
                     <br/>
                     <br/>
-                    I enjoy being challenged and engaging with projects that require me to work outside my comfort
+                    <span className='icons'>&#127919;</span> I enjoy being challenged and engaging with projects that require me to work outside my comfort
                     <br/>
                     zone and knowledge set, as continuing to learn new languages and developmenet techeniques.
                     <br/>
                     <br/>
-                    &#x1F4BB; I'am fluent in many programming language, especialy in JavaScript & Python, as in C++ and Java.
+                    <span className='icons'>&#128200;</span> I'am fluent in many programming language, especialy in JavaScript & Python, as in C++ and Java.
                     <br/>
                     <br/>
-                    My field of Interest's are building new  Web Technologies and Products and also in areas related
+                    <span className='icons'>&#128196;</span> My field of Interest's are building new  Web Technologies and Products and also in areas related
                     <br/>
                     Mobile application and Natural Launguage Processing.
                     <br/>
                     <br/>
-                    whenever possible, i also apply my passion for developing products with multiple diffrent framework
+                    <span className='icons'>&#127756;</span> whenever possible, i also apply my passion for developing products with multiple diffrent framework
                     <br/>
                     and techenologie, like Django, React, Node.js ... 
                 </Text>
