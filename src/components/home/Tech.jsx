df255c0ef058048b0267d2c6805a6f9da06ece71
na0495 <saadmrabet008@gmail.com>
2021-09-09 12:55:29 +0100

    update tech

--- src/components/home/Tech.jsx
+++ src/components/home/Tech.jsx
@@ -7,7 +7,7 @@ export default function Tech() {
     const frontFramework = ['react', 'angular', 'redux', 'bootstrap', 'tailwind']
     const backFramework = ['django', 'django-rest-framework', 'laravel']
     const dataBase = ['postgresql', 'mysql', 'sqlite']
-    const devops = ['git', 'github','netlify', 'google_cloud' ,'docker', 'kubernetes', 'aws', 'heroku']
+    const devops = ['git', 'github','netlify', 'google_cloud' ,'docker'] //, 'kubernetes', 'aws', 'heroku'
     const programming = ['python', 'js', 'typescript', 'html', 'css', 'java', 'c', 'c++', 'php', 'golang', 'bash']
 
     const skillsData = [
