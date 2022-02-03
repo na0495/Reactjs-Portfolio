193bd45f99e34ca29bca2730a4bdfcd0d54558e4
na0495 <saadmrabet008@gmail.com>
2022-02-03 21:52:18 +0000

    add footer to other pages

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -3,6 +3,7 @@ import Particles from '../components/Animation/Particles'
 import { Link } from 'react-router-dom';
 import { Heading, Text} from '@chakra-ui/layout';
 import { makeStyles } from '@material-ui/core';
+import Footer from '../layouts/Footer';
 
 const useStyles = makeStyles((theme) => ({
     title: {
@@ -11,25 +12,33 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
     },
     root: {
-        paddingTop: 10,
-        paddingBottom: 150,
+        height: window.innerHeight / 1.3,
+        // center the content
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // textAlign: 'center',
+        paddingBottom: 175
+
     }
 }));
 
-const NotFound = () => {
+const Soon = () => {
     const classes = useStyles();
     return (
     <Particles>
-    <div className={classes.root}>
-        <Heading as="h1" size="2xl" className={classes.title} isTruncated p={4} mt={250} >
-            Comming Soon ! 
-        </Heading>
-        <Link to="/" className={classes.title} mb={100} mt={25}>
-            <Text fontSize="3xl" color="orange"> Go Home </Text>
-        </Link>
-    </div>
+        <div className={classes.root}>
+            <Heading as="h1" size="2xl" className={classes.title} isTruncated p={4} mt={250} >
+                404 - Page Not Found
+            </Heading>
+            <Link to="/" className={classes.title} mb={100} mt={25}>
+                <Text fontSize="3xl" color="orange"> Go Home </Text>
+            </Link>
+        </div>
+        <Footer/>
     </Particles>
     );
 };
 
-export default NotFound;
+export default Soon;
