905120416bc8134a6a64d1d2b591ebcacf714fa8
na0495 <saadmrabet008@gmail.com>
2022-02-04 18:30:58 +0000

    update the content

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,8 +2,9 @@ import React, { useRef } from 'react'
 import { Box, useColorModeValue, useColorMode } from '@chakra-ui/react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import styles from './styles.module.css'
+import { Posts } from '../components/projects/Posts'
 
-const Page = ({ offset, gradient, onClick, bg }) => {
+const Page = ({ offset, gradient, onClick, bg, mode }) => {
   return (
     <>
       <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
@@ -16,6 +17,10 @@ const Page = ({ offset, gradient, onClick, bg }) => {
 
       <ParallaxLayer className={`${styles.text} ${styles.number}`} offset={offset} speed={0.3}>
         <span>0{offset + 1}</span>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={offset} speed={0.3} onClick={onClick}>
+        <Posts />
       </ParallaxLayer>
     </>
   )
@@ -37,7 +42,7 @@ export default function App() {
     <div style={{ background: '#dfdfdf' }}>
       <Parallax className={styles.container} ref={parallax} pages={3} horizontal>
         <Page offset={0} gradient={colorMode === 'light' ? "lightGreen" : "darkGreen" } bg={useColorModeValue('#C1DEAE', '#519259')} onClick={() => scroll(1)} />
-        <Page offset={1} gradient="teal" onClick={() => scroll(2)} />
+        <Page offset={1} gradient={colorMode === 'light' ? "lightCreme" : "darkCreme" } bg={useColorModeValue('#DBD0C0', '#B68973')}  onClick={() => scroll(2)} />
         <Page offset={2} gradient="tomato" onClick={() => scroll(0)} />
       </Parallax>
     </div>
